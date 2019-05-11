46ebc340cf19e9a1f985bb76fa4eea5c0c44a419
Abner Moran <brain.agam@gmail.com>
2019-05-11 07:25:47 +0200

    blueprint for sandwich detail

--- v1/blueprint.py
+++ v1/blueprint.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from v1.views.bars import BarDetail
 from v1.views.bars import BarList
 from v1.views.bars import SandwichList
+from v1.views.bars import SandwichDetail
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -11,3 +12,4 @@ api = Api(api_bp)
 api.add_resource(BarList, '/v1/bars/')
 api.add_resource(BarDetail, '/v1/bars/<int:id>/')
 api.add_resource(SandwichList, '/v1/bars/<int:bar_id>/sandwiches/')
+api.add_resource(SandwichDetail, '/v1/bars/<int:bar_id>/sandwiches/<int:sandwich_id>/')
