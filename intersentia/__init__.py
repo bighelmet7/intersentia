a6e986cb6f3519f396b7365b2f61899ad17f4260
Abner Moran <brain.agam@gmail.com>
2019-05-11 07:25:25 +0200

    batch support

--- intersentia/__init__.py
+++ intersentia/__init__.py
@@ -13,7 +13,7 @@ def create_app(config_obj='intersentia.config.DevelopmentConfig'):
     # understand the concept of session and scoped_session.
     # Create makemigrations and migrate.
     db.init_app(app)
-    migrate.init_app(app, db=db)
+    migrate.init_app(app, db=db, render_as_batch=True)
 
     from v1.blueprint import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
