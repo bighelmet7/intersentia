ed6f57685037aa3488f162ea2c61801429a8f0eb
Abner Moran <brain.agam@gmail.com>
2019-05-11 06:01:16 +0200

    CRUD and abstract model for inherence between models

---
+++ intersentia/database.py
@@ -0,0 +1,35 @@
+from intersentia.extensions import db
+
+
+class CRUDMixin(object):
+    """
+    CRUDS operations for any SQLAlchemy instance.
+    """
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.iteritems():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+
+class Model(CRUDMixin, db.Model):
+    """
+    Base model with CRUD operations.
+    """
+
+    __abstract__ = True
