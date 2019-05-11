e565f4a9ee2ebd83bc9aa6e2448d58faf18933ea
Abner Moran <brain.agam@gmail.com>
2019-05-11 06:04:12 +0200

    blueprints for the api

---
+++ v1/blueprint.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask_restful import Api
+
+from v1.views.bars import BarDetail
+from v1.views.bars import BarList
+from v1.views.bars import SandwichList
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+api.add_resource(BarList, '/v1/bars/')
+api.add_resource(BarDetail, '/v1/bars/<int:id>/')
+api.add_resource(SandwichList, '/v1/bars/<int:bar_id>/sandwiches/')
