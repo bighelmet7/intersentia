9db9071869eb9748b3f11783b746ba8062a834a2
Abner Moran <brain.agam@gmail.com>
2019-05-12 11:21:48 +0200

    accepts all the paths for react

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from intersentia import create_app
+from flask import render_template
 
 app = create_app()
 
 @app.route('/ping/')
 def ping():
     return 'Pong'
+
+# INFO: Code snippet: http://flask.pocoo.org/snippets/57/
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
